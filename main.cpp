1f879d7e1ac4171e7acda2511473b370392827f9
rainppy <84164656+rainppy@users.noreply.github.com>
2022-06-06 20:25:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,80 @@
-void Fenjie(ZZ n)
-{
-	if (ProbPrime(n))
-	{
-		factor[++total] = n;
-		return;
-	}
-	ZZ a, pr;
+// sm3_str.c
+#pragma comment(lib, "libssl.lib")
+#pragma comment(lib, "libcrypto.lib")
+#define _CRT_SECURE_NO_WARNINGS
+#define HASH_RESULT_LEN 32
+#define Size 4
+#include <openssl/evp.h>
+#include<stdio.h>
+#include <string.h>
 
-	pr = Pollard_rho(n);
-	Fenjie(pr);
-	Fenjie(n / pr);
-}
-ZZ Pollard_rho(ZZ n)
+unsigned int hash_str(const char* str, const size_t len,
+    unsigned char* hash_result)
 {
-	ZZ p;//·µ»ØµÄÒò×Ó
-	for (int i = 0;i < 100;i++)
-	{
-		ZZ a, y, x;
-		RandomBits(a, 4);
-		a = a % n;
-		x = a;
-		y = (MulMod(x, x, n) + 1) % n;
-		while (true)
-		{
-			x = (MulMod(x, x, n) + 1) % n;
-			y = (MulMod(y, y, n) + 1) % n;
-			y = (MulMod(y, y, n) + 1) % n;
-			if (x == y)
-				break;
-			p = GCD(abs(x - y), n);
-			if (p != 1)
-			{
-				cout << "x=" << x << "y=" << y << endl;
-				return p;
-			}
-		}
-	}
-	return n;
+    unsigned int ret;
+    const EVP_MD* alg = EVP_sm3();
+    EVP_Digest(str, len, hash_result, &ret, alg, NULL);
+    return ret;
+}
+int strcmp_n(unsigned char str1[], unsigned char str2[], int n)
+{
+    //å½“ç›¸ç­‰è¾“å‡º1ï¼Œä¸ç­‰è¾“å‡º0
+    for (int i = 0; i < n; i++)
+    {
+        if (str1[i] != str2[i])
+            return 0;
+    }
+    return 1;
+}
+int main(int argc, char const* argv[])
+{
+
+    unsigned char str0[20] = "hello world";
+    unsigned char str[HASH_RESULT_LEN];
+    unsigned char str1[HASH_RESULT_LEN];
+    strcpy(str, str0);
+    strcpy(str1, str0);
+    int i = 1;
+    while (1)
+    {
+        hash_str(str, HASH_RESULT_LEN, str);
+        hash_str(str1, HASH_RESULT_LEN, str1);
+        hash_str(str1, HASH_RESULT_LEN, str1);
+        if (strcmp_n(str, str1, Size))
+            break;
+        i++;
+    }
+    for (int i = 0; i < HASH_RESULT_LEN; i++) {
+        printf("%02x", str[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < HASH_RESULT_LEN; i++) {
+        printf("%02x", str1[i]);
+    }
+    printf("\n");
+    for (int j = 1; j <= i; j++)
+    {
+        unsigned char temp[HASH_RESULT_LEN];
+        unsigned char temp1[HASH_RESULT_LEN];
+        hash_str(str, HASH_RESULT_LEN, temp);
+        hash_str(str1, HASH_RESULT_LEN, temp1);
+        if (strcmp_n(temp, temp1, Size))
+            break;
+        else
+        {
+            hash_str(str, HASH_RESULT_LEN, str);
+            hash_str(str1, HASH_RESULT_LEN, str1);
+        }
+    }
+    for (int i = 0; i < HASH_RESULT_LEN; i++) {
+        printf("%02x", str[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < HASH_RESULT_LEN; i++) {
+        printf("%02x", str1[i]);
+    }
+    printf("\n");
+
+
+    return 0;
 }
