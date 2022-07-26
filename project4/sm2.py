a338518a0c74dd0c48b7fa5ad72292739d50f986
rainppy <84164656+rainppy@users.noreply.github.com>
2022-07-26 15:29:36 +0800

    修改

--- project4/sm2.py
+++ project4/sm2.py
@@ -183,17 +183,5 @@ class CryptSM2(object):
             return None
         else:
             return '%064x%064x' % (R,S)
-if __name__ =="__main__":
-    private_key = '00B9AB0B828FF68872F21A837FC303668428DEA11DCD1B24429D0C99E24EED83D5'
-    public_key = 'B9C9A6E04E9C91F7BA880429273747D7EF5DDEB0BB2FF6317EB00BEF331A83081A6994B8993F3F5D6EADDDB81872266C87C018FB4162F5AF347B483E24620207'
-    sm2_crypt = CryptSM2(
-        public_key=public_key, private_key=private_key)
-    data = b"111"
-    print("-----------------test sign and verify---------------")
-    random_hex_str = random_hex(sm2_crypt.para_len)
-    sign = sm2_crypt.sign(data, random_hex_str)
-    print('sign:%s' % sign)
-    verify = sm2_crypt.verify(sign, data)
-    print('verify:%s' % verify)
-    assert verify
+
 
