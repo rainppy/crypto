83904dae5e6c5c4fd522ddd542b366f5d8614bf5
rainppy <84164656+rainppy@users.noreply.github.com>
2022-07-30 14:11:46 +0800

    rename,以防调用包冲突

--- project2/attack.py
+++ project2/attack.py
@@ -1,12 +1,12 @@
-import sm3
+import sm3_2
 
 bytes_to_list = lambda data: [i for i in data]
 def check_res(extend_msg,hash_res):
     B0 = bytes_to_list(b"secretkeyBest wishes!")
-    sm3.Padding(B0)
+    sm3_2.Padding(B0)
     Bi = bytes_to_list(bytes(extend_msg,encoding="ascii"))
     B0.extend(Bi)
-    if(sm3.sm3_hash(B0)==hash_res):
+    if(sm3_2.sm3_hash(B0)==hash_res):
         print("success!")
     else:
         print("fail!")
@@ -15,7 +15,7 @@ def check_res(extend_msg,hash_res):
     
 if __name__ == '__main__':
     origin_msg = "secretkeyBest wishes!"
-    Vi = sm3.sm3_hash(bytes_to_list(bytes(origin_msg,encoding="ascii")))
+    Vi = sm3_2.sm3_hash(bytes_to_list(bytes(origin_msg,encoding="ascii")))
     extend_msg = "Attack!"
     Bi = bytes_to_list(bytes(extend_msg,encoding="ascii"))
     length = len(Bi) + 64
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     #fill in the reverse order
     for m in length_bit_arr[::-1]:
         Bi.append(m)
-    hash_res = sm3.CF(Vi, Bi)
+    hash_res = sm3_2.CF(Vi, Bi)
     result=""
     for i in hash_res:
         result = '%s%08x' % (result, i)
