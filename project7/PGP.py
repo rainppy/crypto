b3ccefd93f52b756f22596aaaf4414a072d5f642
rainppy <84164656+rainppy@users.noreply.github.com>
2022-07-30 15:14:08 +0800

    修改文件名，以防包引用错误

--- project7/PGP.py
+++ project7/PGP.py
@@ -1,6 +1,4 @@
-import sm2
-import sm3
-import func
+import sm2_3
 from sm4 import CryptSM4, SM4_ENCRYPT, SM4_DECRYPT
 class sender(object):
     def __init__(self, sm2_crypt):
@@ -35,7 +33,7 @@ class receiver(object):
 if __name__ =="__main__":
     private_key = '00B9AB0B828FF68872F21A837FC303668428DEA11DCD1B24429D0C99E24EED83D5'
     public_key = 'B9C9A6E04E9C91F7BA880429273747D7EF5DDEB0BB2FF6317EB00BEF331A83081A6994B8993F3F5D6EADDDB81872266C87C018FB4162F5AF347B483E24620207'
-    sm2_crypt = sm2.CryptSM2(public_key=public_key, private_key=private_key)
+    sm2_crypt = sm2_3.CryptSM2(public_key=public_key, private_key=private_key)
 
     #初始化收发对象
     Alice = sender(sm2_crypt)
